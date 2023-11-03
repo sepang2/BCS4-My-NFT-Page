2e87dfb859f5a9a4f333312ea278826301acea4e
Sepang2 <sepang_2@naver.com>
2023-11-03 17:58:53 +0900

    quotes

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const clock = document.querySelector(".clock");
+const quotes = document.querySelector(".quotes");
+const newQuotes = document.querySelector(".new-quotes");
+const newQuotesInput = document.querySelector(".new-quotes-input");
 
 function getTime() {
   const date = new Date();
@@ -18,3 +21,44 @@ function getTime() {
 
 getTime();
 setInterval(getTime, 1000);
+
+const QUOTES_LIST = "quotesList";
+
+function getQuotes() {
+  let savedQuotes = localStorage.getItem(QUOTES_LIST);
+
+  if (!savedQuotes) {
+    localStorage.setItem(QUOTES_LIST, JSON.stringify(["~워라밸~"]));
+
+    savedQuotes = localStorage.getItem(QUOTES_LIST);
+  }
+
+  let parsedQuotes = JSON.parse(savedQuotes);
+  quotes.innerText =
+    parsedQuotes[Math.floor(Math.random() * parsedQuotes.length)];
+}
+
+getQuotes();
+
+function onClickNewQuotes() {
+  if (!newQuotesInput.value) return;
+
+  // 로컬 스토리지에 저장
+  let savedQuotes = localStorage.getItem(QUOTES_LIST);
+  let parsedQuotes = JSON.parse(savedQuotes);
+
+  parsedQuotes.push(newQuotesInput.value);
+
+  localStorage.setItem(QUOTES_LIST, JSON.stringify(parsedQuotes));
+
+  // 현재 페이지 반영
+  quotes.innerText = newQuotesInput.value;
+  newQuotesInput.value = "";
+  quotes.style.display = "block";
+  newQuotes.style.display = "none";
+}
+
+function onClickQuotes() {
+  quotes.style.display = "none";
+  newQuotes.style.display = "block";
+}
