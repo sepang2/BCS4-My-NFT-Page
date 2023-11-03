340941f15a0ed5e357420cab120a553e817c25f6
Sepang2 <sepang_2@naver.com>
2023-11-03 14:17:47 +0900

    clock

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+const clock = document.querySelector(".clock");
 
+let seconds = 11;
+
+function getTime() {
+  const date = new Date();
+
+  let hrs = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  let amPm = "AM";
+
+  if (hrs > 11) amPm = "PM";
+  if (hrs > 12) hrs %= 12;
+
+  clock.innerText = `${amPm} ${String(hrs).padStart(2, "0")}:${String(
+    min
+  ).padStart(2, "0")}:${String(sec).padStart(2, "0")}`;
+}
+
+getTime();
+setInterval(getTime, 1000);
